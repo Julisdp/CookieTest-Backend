6276f1e0ffa6df7fb31e923bacc339263b42d0b6
IgnacioParra19 <ignacioparraingaramo@gmail.com>
2024-06-04 16:27:05 -0300

    Base de datos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config(); 
+
 const app = express();
 const port = process.env.PORT || 2000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')}
+);
+
+mongoose
+.connect(process.env.MONGODB_URI)
+.then(() => console.log('Conectado a MongoDB Atlas'))
+.catch((error) => console.error(error));
 
 app.listen(port, () => console.log('listening on port' + port));
