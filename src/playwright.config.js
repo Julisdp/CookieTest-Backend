196fb3d3e06eff1b96d3596d2163d721cfc8510b
IgnacioParra19 <ignacioparraingaramo@gmail.com>
2024-06-09 19:45:49 -0300

    front conectado al back re hecho

--- src/playwright.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require('@playwright/test');
-
-module.exports = defineConfig({
-  testDir: './tests', // Directorio donde se encuentran tus pruebas
-  use: {
-    headless: true, // Opcional: si quieres ejecutar las pruebas en modo headless
-  },
-});
